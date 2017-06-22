a46bd19bccc33dcfa8bbae6f392f7cd496f59775
LaurensIsta <laurens-i@Hotmail.com>
2017-06-22 13:56:30 +0200

    Interfaces Level/Area
    Added interfaces for Level and area

---
+++ Kru-Puk/Kru-Puk/IArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    interface IArea
+    {
+        string CheckArea(); // returns Inside/Outside
+    }
+}
