ccdf55ab13e5bbe070e37aa1bef2ad445a2e8f83
Dionykn <dionklein12@hotmail.com>
2017-06-22 14:08:50 +0200

    Add interface door/stair

---
+++ Kru-Puk/Kru-Puk/IStairs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementen_van_classes_en_interfaces
+{
+    interface IStairs
+    {
+    }
+}
