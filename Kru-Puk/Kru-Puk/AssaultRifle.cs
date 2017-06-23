0514d505771b817b47196e310eb981e50bf0b9c9
LaurensIsta <laurens-i@Hotmail.com>
2017-06-23 09:47:57 +0200

    Assault Rifle edit
    Assault Rifle class edit

--- Kru-Puk/Kru-Puk/AssaultRifle.cs
+++ Kru-Puk/Kru-Puk/AssaultRifle.cs
@@ -9,20 +9,49 @@ using Microsoft.Xna.Framework;
 
 namespace Kru_Puk
 {
-    class AssaultRifle: IWeapon //ClassNotImplemented
+    class AssaultRifle: IWeapon //TODO InterfaceNotImplemented
     {
         private int MaxClipAmount;
         private int ClipAmount;
+        private int AmmoInClip;
         private Projectile projectile;
 
-        AssaultRifle(int MaxClipAmount, int ClipAmount, Projectile projectile)
+        AssaultRifle(int MaxClipAmount, int ClipAmount, int AmmoInClip, Projectile projectile)
         {
             this.MaxClipAmount = MaxClipAmount;
             this.ClipAmount = ClipAmount;
+            this.AmmoInClip = AmmoInClip;
             this.projectile = projectile;
         }
 
+        public void Draw(SpriteBatch spritebatch)
+        {
+            throw new NotImplementedException();
+        }
 
-        
+        public void Reload(int AmmoInClip)
+        {
+            AmmoInClip = 30;
+            ClipAmount = ClipAmount - 1;
+        }
+
+        public void Update(float dt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Use()
+        {
+            if (AmmoInClip > 0)
+            {
+                AmmoInClip = AmmoInClip - 1;
+                //TODO Create new projectile
+            }
+
+            else if (AmmoInClip <= 0 && ClipAmount > 0)
+            {
+                this.Reload(AmmoInClip);
+            }
+        }
     }
 }
